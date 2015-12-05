6bbf9ac90a0930f8ee897ce1c4676c0b4b69bb74
agangal <ashish.gangal1990@gmail.com>
2015-12-05 15:15:09 -0800

    Pushing the entire project.

---
+++ TeamSnapAuth/LibraryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamSnapAuth.Library;
+using Windows.Storage;
+
+namespace TeamSnapAuth
+{
+    public class LibraryManager
+    {
+        private string clientID;
+        private string authEndPoint;
+        private string callBackURL;
+        public Library_APICall APILibrary;
+        public Library_TokenAuth TokenLibrary;
+        public LibraryManager()
+        {
+            
+            APILibrary = new Library_APICall();
+            TokenLibrary = new Library_TokenAuth();
+        }
+
+        public async Task<String> getToken()
+        {
+            string resp = await TokenLibrary.getTeamSnapToken(authEndPoint, clientID, callBackURL);
+            Debug.WriteLine("access token : " + resp);
+            return resp;
+        }
+
+        public async Task<String> callAPI(string access_token, string api)
+        {
+            //string access_token = (string)ApplicationData.Current.LocalSettings.Values["Tokens"];
+            string resp = await APILibrary.apiCall(access_token, api);
+            return resp;
+        }
+    }
+}
---
+++ TeamSnapAuth/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TeamSnapAuth
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            LibraryManager Library = new LibraryManager();
+            string resp = null;
+            string api = API.Text;
+            string access_token = await Library.getToken();
+            resp = await Library.callAPI(access_token, api);
+            APIResult.Text = resp;
+        }
+    }
+}
