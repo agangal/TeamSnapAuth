650fab7aad715569f1f868c80da15c9336169f9b
agangal <ashish.gangal1990@gmail.com>
2015-12-05 15:08:22 -0800

    Library to make API Calls.

---
+++ TeamSnapAuth/Library/Library_APICall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Windows.Web.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http.Headers;
+using Windows.ApplicationModel.DataTransfer;
+
+namespace TeamSnapAuth.Library
+{
+    public class Library_APICall
+    {
+        // make an API call
+        public async Task<string> apiCall(String access_token, String href)
+        {
+            HttpClient httpClient = new HttpClient();
+            Debug.WriteLine("Created httpclient. " + href);
+            httpClient.DefaultRequestHeaders.Authorization = new HttpCredentialsHeaderValue("Bearer", access_token);
+            httpClient.DefaultRequestHeaders.Accept.Add(new HttpMediaTypeWithQualityHeaderValue("application/json"));
+            Debug.WriteLine("Awaiting http request after this");
+            var httpResponseMessage = await httpClient.GetAsync(new Uri(href));
+            Debug.WriteLine("Done awaiting http request");
+            string apiResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+            Debug.WriteLine("Done reading response as string : " + apiResponse);
+
+            var dataPackage = new DataPackage();
+            dataPackage.SetText(apiResponse);
+            Clipboard.SetContent(dataPackage);
+            return apiResponse;
+        }
+    }
+}
